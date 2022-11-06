cc2cd4b55f1eb5f13b63ad451d565d0fc1667cc9
Zeeshankhan29 <zeeshankhan29khan@gmail.com>
2022-11-06 17:54:33 +0530

    Final commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def test():
-    return 'This is My First CI-CD pipeline update'
+    return 'This is My First CI-CD pipeline'
 
 
 if __name__ == "__main__":
